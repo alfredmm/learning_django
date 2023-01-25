e642a3c11cba1cb374a6945f6782baa6efbeb5b7
alfredmm <alfredmutua2030@gmail.com>
2023-01-26 01:50:43 +0300

    Version 2

--- learndjango/settings.py
+++ learndjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8^3a(1!+!isl^jey1w@6(xmd_5_ehkb#p7qn9ym_d(h-fo-xy@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'learndjango.urls'
@@ -118,7 +119,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'productionfiles'
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'mystaticfiles'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- members/views.py
+++ members/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from .models import Member
+from django.db.models import Q
 
 # Create your views here.
 def members(request):
@@ -25,8 +26,17 @@ def main(request):
     return HttpResponse(template.render())
 
 def testing (request):
+    #mydata = Member.objects.all().order_by('last_name', "-id").values()
+    #mydata = Member.objects.all().order_by('-first_name').values() - Descending use Minus sign
+    #mydata = Member.objects.all().order_by('first_name').values() - Ascending
+    #mydata = Member.objects.filter(first_name__startswith = 'L').values()
+    #mydata = Member.objects.filter(Q(first_name= 'Alfred') | Q(first_name = 'Mike'))
+    #mydata = Member.objects.filter(first_name = 'Alfred') | Member.objects.filter(first_name= "Mike")
+    #mydata = Member.objects.filter(first_name = 'Alfred')
+    #filtered_members = Member.objects.filter(first_name= 'Alfred')
     template = loader.get_template('template.html')
     context = {
-        'fruits': ['Cherry', 'Banana', 'Apple'],
+        #'mymembers' : mydata,
+        'fruits': ['Apple', 'Orange', 'Lemon', 'Cherries'],
     }
     return HttpResponse(template.render(context, request))
